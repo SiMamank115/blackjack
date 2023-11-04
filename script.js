f1e672f94ea67b56036607c967a309d54ea0a673
Faeest <faeest.115@gmail.com>
2023-11-04 17:46:43 +0700

    button animation

--- script.js
+++ script.js
@@ -8,61 +8,85 @@ const CARD_HEIGHT = 100;
 const FRAME_RATE = 60;
 const AnimationTiming = {
     tableCards: {
+        //* timing cards squares animation
         frame: 0,
         increment: _.round(FRAME_RATE / 12),
     },
     tableInfo: {
+        //* timing middle squares animation
         frame: _.round(FRAME_RATE * 0.5),
+        increment: _.round(FRAME_RATE / 6),
     },
 };
+const GameAnimation = {};
 const GameProperties = {};
 const GameObjects = [];
-const tableAnimation = animS.newAnimS(p5);
 //* utility
+function searchObject(classname = "") {
+    return _.filter(GameObjects, (e) => e.constructor.name == classname);
+}
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 //* core function
 function preload() {
     GameProperties.GUI = {
-        cardPos: [],
-        infoPos: [],
+        cardPos: [], //* 10 squares for players cards [x,y]
+        infoPos: [], //* middle square position [x,y]
+        pilePos: [],
+        background: [255, 225], //* background animation [from,to]
     };
-    let margin = CARD_WIDTH * 0.5;
-    let midWidth = CARD_WIDTH * 2.75 + margin;
+    GameAnimation.pile = { x: 0, y: -200 };
+    GameAnimation.bet = [
+        { x: 400, y: 200 },
+        { x: 400, y: 200 },
+        { x: 400, y: 200 },
+        { x: 400, y: 200 },
+        { x: 400, y: 200 },
+        { x: 400, y: 200 },
+    ];
+    GameAnimation.cards = [];
+    let margin = CARD_WIDTH * 0.5; //* player cards margin (right margin)
+    let midWidth = CARD_WIDTH * 2.75 + margin; //* middle squares width
     for (let i = 0; i < 10; i++) {
         if (i < 5) {
             GameProperties.GUI.cardPos.push([50 + (CARD_WIDTH + margin) * i, 50]);
         } else {
+            if (i == 5) GameProperties.GUI.pilePos = [55 + (CARD_WIDTH + margin) * i, 50];
             GameProperties.GUI.cardPos.push([50 + (CARD_WIDTH + margin) * (i - 5), Board[1] - 50 - CARD_HEIGHT]);
         }
     }
     for (let i = 0; i < 2; i++) {
         GameProperties.GUI.infoPos.push([50 + (midWidth + margin) * i, 50 + CARD_HEIGHT + 50]);
     }
-    GameProperties.cardsPile = new CardsPile();
-    GameProperties.cardsPile.newPile();
     for (let i = 0; i < 4; i++) {
         for (let x = 2; x <= 14; x++) {
-            Sprites["cards"][i].push(loadImage(`/assets/${x}.${i + 1}.png`, undefined, () => alert("error")));
+            Sprites["cards"][i].push(loadImage(`/assets/${x}.${i + 1}.png`, undefined, () => alert("error loading content! please reload or report it to the dev!")));
         }
     }
     let backSideSkin = _.random(1, 8, false);
-    Sprites["backside"] = loadImage(`/assets/backside/Back${backSideSkin}.png`, undefined, () => alert("error"));
+    Sprites["backside"] = loadImage(`/assets/backside/Back${backSideSkin}.png`, undefined, () => alert("error loading content! please reload or report it to the dev!"));
 }
 
 function setup() {
     frameRate(FRAME_RATE);
     createCanvas(...Board);
-    GameObjects.push(new Cards(2, 0, 0, 0, 10));
-    GameObjects.push(new Cards(14, 2, 50, 0, 100));
+    GameObjects.push(new Game());
+    GameObjects.push(new Bet());
+    let bet = searchObject("Bet")?.[0];
+    if (bet) {
+        for (let i = 0; i < bet.buttons.length; i++) {
+            gsap.to(GameAnimation.bet[i], { x: 0, y: 0, delay: 2 + i * 0.1 });
+        }
+    }
+    gsap.to(GameAnimation.pile, { y: 0, delay: 2 });
 }
 
 function draw() {
-    background(225);
-    // renderGameObject();
-    // renderGameProperties();
+    background(GameProperties.GUI.background);
     renderTable();
+    renderGameObject();
+    renderGameProperties();
     renderGameInfo();
 }
 //* rendering function
@@ -70,6 +94,7 @@ function renderGameObject(zIndex = true) {
     let newGameObjects = zIndex ? _.sortBy(GameObjects, ["zIndex"]) : GameObjects;
     newGameObjects.forEach((e) => {
         e?.render?.();
+        e?.draw?.();
     });
 }
 function renderGameProperties(zIndex = false) {
@@ -79,7 +104,7 @@ function renderGameProperties(zIndex = false) {
     });
 }
 function renderGameInfo() {
-    if (frameCount && frameCount % 10 == 0) {
+    if (frameCount && !(frameCount % 20)) {
         document.querySelector(".frame").textContent = `FPS : ${round(frameRate())}`;
     }
 }
@@ -88,25 +113,46 @@ function renderTable() {
     let tableInfo = AnimationTiming.tableInfo;
     let margin = CARD_WIDTH * 0.5;
     let midWidth = CARD_WIDTH * 2.75 + margin;
+    let padding = 3;
     push();
     strokeWeight(2);
     stroke(180);
     noFill();
     for (let i = 0; i < 10; i++) {
         if (i < 5 && frameCount >= tableCards.frame + tableCards.increment * i) {
-            animS.rect(`r${i + 1}`, FRAME_RATE * 0.3, GameProperties.GUI.cardPos[i][0],GameProperties.GUI.cardPos[i][1], CARD_WIDTH, CARD_HEIGHT);
+            animS.rect(`r${i + 1}`, FRAME_RATE * 0.3, GameProperties.GUI.cardPos[i][0] + padding, GameProperties.GUI.cardPos[i][1] + padding, CARD_WIDTH - padding * 2, CARD_HEIGHT - padding * 2);
         } else if (i >= 5 && frameCount >= tableCards.frame + tableCards.increment * i) {
-            animS.rect(`r${i + 1}`, FRAME_RATE * 0.3, GameProperties.GUI.cardPos[i][0],GameProperties.GUI.cardPos[i][1], CARD_WIDTH, CARD_HEIGHT);
+            animS.rect(`r${i + 1}`, FRAME_RATE * 0.3, GameProperties.GUI.cardPos[i][0] + padding, GameProperties.GUI.cardPos[i][1] + padding, CARD_WIDTH - padding * 2, CARD_HEIGHT - padding * 2);
         }
     }
     for (let i = 0; i < 2; i++) {
-        if (frameCount >= tableInfo.frame + i * 10) {
+        if (frameCount >= tableInfo.frame + tableInfo.increment * i) {
             animS.rect(`r${i + 11}`, FRAME_RATE * 0.6, GameProperties.GUI.infoPos[i][0], GameProperties.GUI.infoPos[i][1], midWidth, CARD_HEIGHT);
+        }
+    }
+    if (!(frameCount % 3) && GameProperties.GUI.background[0] != GameProperties.GUI.background[1]) {
+        if (GameProperties.GUI.background[0] - GameProperties.GUI.background[1] > 0) {
+            GameProperties.GUI.background[0] -= 1;
+        } else {
+            GameProperties.GUI.background[0] += 1;
         }
     }
     pop();
 }
 //* classes
+class Game {
+    constructor() {
+        this.started = false;
+        this.player = new Player();
+        this.bot = new Player();
+        this.cards = new CardsPile();
+    }
+    render() {
+        this.player.render();
+        this.bot.render(true);
+        this.cards.render();
+    }
+}
 class GameObject {
     constructor() {
         this.x = 0;
@@ -118,9 +164,43 @@ class GameObject {
     }
     render() {}
 }
+class Bet {
+    constructor() {
+        this.value = [1, 2, 5, 10, 100, 500];
+        this.pos = [];
+        for (let i = 0; i < 6; i++) {
+            this.pos.push([Board[0] - (50 - (i % 3) * 60) - CARD_WIDTH * 2.575, 50 * 2 + CARD_HEIGHT - 5 + _.floor(i / 3) * 60]);
+        }
+        this.buttons = [];
+        for (let i = 0; i < 6; i++) {
+            this.buttons.push(
+                new Button({
+                    basex: this.pos[i][0],
+                    basey: this.pos[i][1],
+                    x: this.pos[i][0],
+                    y: this.pos[i][0],
+                    cornerRadius: 999,
+                    color: "#ffffff00",
+                    width: 50,
+                    height: 50,
+                    text: `$${this.value[i]}`,
+                    stroke: "#B4B4B4",
+                    id: `bet-${this.value[i]}`,
+                })
+            );
+        }
+    }
+    render() {
+        this.buttons.forEach((e, i) => {
+            e.x = e.basex + GameAnimation.bet[i].x;
+            e.y = e.basey + GameAnimation.bet[i].y;
+            e.draw();
+        });
+    }
+}
 class Player {
     constructor() {
-        this.hand = [new Cards()];
+        this.hand = [];
         this.value = 0;
         this.turn = false;
     }
@@ -130,18 +210,32 @@ class Player {
             result[0] += e.ace ? 1 : e.value;
             result[1] += e.ace ? 11 : e.value;
         });
-        return result;
+        this.value = sort(_.filter(result, (e) => e <= 21));
+    }
+    render(up = false) {
+        this.hand.forEach((e, x) => {
+            let pos = GameProperties.GUI.cardPos[(up ? 0 : 5) + x];
+            e.render(...pos);
+        });
     }
 }
 class CardsPile {
     constructor() {
         this.cards = [];
         this.picked = [];
-        this.x = 0;
-        this.y = 0;
+        this.x = GameProperties?.GUI?.pilePos?.[0] || 0;
+        this.y = GameProperties?.GUI?.pilePos?.[1] || 0;
+        this.maxX = Board[0] - 50 - CARD_WIDTH;
         this.width = CARD_WIDTH;
         this.height = CARD_HEIGHT;
-        this.piledCards = 6;
+        this.margin = 8;
+        this.piledCards = ceil((this.maxX - this.x) / this.margin);
+        for (let i = 1; i <= 4; i++) {
+            for (let x = 2; x <= 14; x++) {
+                this.cards.push([x, i]);
+            }
+        }
+        this.cards = shuffle(_.shuffle(this.cards));
     }
     newPile() {
         this.cards = [];
@@ -154,7 +248,7 @@ class CardsPile {
     }
     render() {
         for (let i = 0; i < (this.cards.length > this.piledCards ? this.piledCards : this.cards.length); i++) {
-            image(Sprites["backside"], this.x + i * 10, this.y, this.width, this.height);
+            image(Sprites["backside"], this.maxX - i * this.margin + (GameAnimation.pile.x ?? 0), this.y + (GameAnimation.pile.y ?? 0), this.width, this.height);
         }
     }
 }
@@ -172,7 +266,45 @@ class Cards extends GameObject {
         this.zIndex = zIndex;
         this.image = Sprites["cards"][cardType][cardNumber - 2];
     }
-    render() {
-        image(this.image, this.x, this.y, this.width, this.height);
+    render(x, y) {
+        image(this.image, x ?? this.x, y ?? this.y, this.width, this.height);
     }
 }
+
+class Button extends Clickable {
+    constructor(
+        options = {
+            x: 100,
+            y: 100,
+            width: 60,
+            height: 30,
+            color: "#FFFFFF00",
+            cornerRadius: 0,
+            strokeWeight: 2,
+            stroke: "#000000",
+            text: "Press Me",
+            textColor: "#000000",
+            textSize: 12,
+            textFont: "sans-serif",
+            textScaled: false,
+            image: null,
+            fitImage: false,
+            imageScale: 1,
+            tint: null,
+            noTint: true,
+            filter: null,
+        }
+    ) {
+        super();
+        if (typeof options == "object") {
+            _.forEach(options, (e, x) => {
+                this[x] = options[x];
+            });
+        }
+        if (GameObjects && !this.id) {
+            this.id = `button-${_.filter(GameObjects, (e) => e.constructor.name == "Button").length + 1}`;
+        }
+    }
+}
+
+// tv
